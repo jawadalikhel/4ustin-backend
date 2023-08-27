f2a0a9367e7bbdb7e2caadf1c5c1a13d4d4b444a
jawadalikhel <jawadalikhel18@gmail.com>
2023-08-26 23:32:02 -0500

    starting with basic backend structure for 4ustin

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const placesRoutes = require('./routes/places-routes');
+
+const app = express();
+
+app.use('/api/places' ,placesRoutes);
+
+
+app.listen(5000, ()=>{
+    console.log('Server Running On Port 5000');
+});
